a01a05a220209ac89e270f3b312ae3af6aefa850
purnima shrivastava <purnimashrivastava05@gmail.com>
2026-01-03 17:53:20 +0530

    Update compose.py

--- datasets/pipelines/compose.py
+++ datasets/pipelines/compose.py
@@ -1,1 +1,16 @@
+class Compose:
+    """
+    Compose a sequence of transforms.
+
+    Each transform takes a `results` dictionary,
+    modifies it, and returns it.
+    """
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, results):
+        for transform in self.transforms:
+            results = transform(results)
+        return results
 
